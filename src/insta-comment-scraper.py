a5db3d7d87d7687b78db827231de02321626d8c5
Pavankumar Gurazada <pavankumar.gurazada@gmail.com>
2023-12-01 09:19:55 +0530

    data collection with comments

--- src/insta-comment-scraper.py
+++ src/insta-comment-scraper.py
@@ -19,22 +19,41 @@ from instaloader import Instaloader
 # The input file contains ids of users to collect data from
 # one instagram id per line
 
-input_file = os.path.join('data', 'raw', '2019-03-31_instagram-ids.txt')
+input_file = os.path.join('testdata', 'ids.txt')
 
 with open(input_file, 'r') as f:
     ids_list = f.read()
     ids_list = ids_list.split('\n')
 
-L = Instaloader(dirname_pattern=os.path.join('data', 'raw', '{profile}'),
-                filename_pattern="{shortcode}",
-                download_pictures=True,
-                download_videos=False,
-                download_video_thumbnails=False,
-                download_geotags=False,
-                download_comments=True,
-                save_metadata=False,
-                compress_json=False,
-                post_metadata_txt_pattern="{profile},{date_utc},{shortcode},{likes},{comments}")
+FIELD_SEPARATOR = "xxFLDxx"
+
+META_DATA_PATTERN = (
+    "{shortcode}" + FIELD_SEPARATOR +
+    "{date_utc}" + FIELD_SEPARATOR +
+    "{profile}" + FIELD_SEPARATOR +
+    "{caption}" + FIELD_SEPARATOR + 
+    "{likes}" + FIELD_SEPARATOR + 
+    "{comments}"
+)
+
+# login with instaloader -l "your-instagram-login"
+# the session cookie will be saved to ~/.config/instaloader/session-your-username 
+LOGIN_USER_NAME = "your-instagram-username"
+
+L = Instaloader(
+    dirname_pattern=os.path.join('testdata', '{profile}'),
+    filename_pattern="{shortcode}",
+    download_pictures=True,
+    download_videos=False,
+    download_video_thumbnails=False,
+    download_geotags=False,
+    download_comments=True,
+    save_metadata=False,
+    compress_json=False,
+    post_metadata_txt_pattern=META_DATA_PATTERN
+)
+
+L.load_session_from_file(LOGIN_USER_NAME)
 
 for username in ids_list:
     print("Downloading data for :", username)
